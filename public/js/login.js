feb2d22f09f96ef1b1b1326eb6de6e73ade50fb6
Clara-Quispe-Chileno <242290600+Clara-Quispe-Chileno@users.noreply.github.com>
2026-05-07 15:44:59 +0000

    Instalando npm install @supabase/supabase-js

--- public/js/login.js
+++ public/js/login.js
@@ -47,7 +47,7 @@ document.addEventListener('DOMContentLoaded', () => {
             sessionStorage.setItem('ds_logged', 'true');
             sessionStorage.setItem('ds_user', data.user.id);
             //sessionStorage.setItem('ds_email', data.user.email);
-            window.location.href = 'dashboard.php?user_id=' + data.user.id;
+            window.location.href = 'dash.php?user_id=' + data.user.id;
         } catch (err) {
             console.error(err);
             alert('Error al iniciar sesión: ' + err.message);
